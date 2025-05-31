e0bf71ec553b215be0c95d14f1a5d5e04a0d446a
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:37:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-/**
-* @file main.cpp
-* @description uygulamanın ana ekran kısmı
-* @course 1. Öğretim B grubu
-* @assignment 2. Ödev
-* @date 16.12.24
-* @author Didem Reyhan AKAN b231210002@sakarya.edu.tr
-*/
 
 #include "BST.hpp"
 #include "ListedList.hpp"
