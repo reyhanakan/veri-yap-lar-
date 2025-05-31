a655b16093cf57332bdf7e883e163c1c8d2a2bbc
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:37:46 +0300

    Update Node.hpp

--- include/Node.hpp
+++ include/Node.hpp
@@ -1,11 +1,4 @@
-/**
-* @file Node.hpp
-* @description düğüm tanımı
-* @course 1. Öğretim B grubu
-* @assignment 2. Ödev
-* @date 16.12.24
-* @author Didem Reyhan AKAN b231210002@sakarya.edu.tr
-*/
+
 #ifndef NODE_HPP
 #define NODE_HPP
 
