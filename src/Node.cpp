a6cfeca536e782af581cf83807bcb3dd1dfa6039
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:37:02 +0300

    Update Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,11 +1,4 @@
-/**
-* @file Node.cpp
-* @description node yapıcı kısmı
-* @course 1. Öğretim B grubu
-* @assignment 2. Ödev
-* @date 16.12.24
-* @author Didem Reyhan AKAN b231210002@sakarya.edu.tr
-*/
+
 #include "Node.hpp"
 
 Node::Node() {
