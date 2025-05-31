92bec344ab3ee1d259cf2cda3c9222337efe23dc
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:36:50 +0300

    Update ListedList.cpp

--- src/ListedList.cpp
+++ src/ListedList.cpp
@@ -1,11 +1,4 @@
-/**
-* @file ListedList.cpp
-* @description bağlı liste adres yazma kısmı
-* @course 1. Öğretim B grubu
-* @assignment 2. Ödev
-* @date 16.12.24
-* @author Didem Reyhan AKAN b231210002@sakarya.edu.tr
-*/
+
 #include "ListedList.hpp"
 #include "BST.hpp"
 #include "Node.hpp"
